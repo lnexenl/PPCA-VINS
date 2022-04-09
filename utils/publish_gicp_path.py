6433e1a37b30a7a5bf3df7712c5d37938e6b91f2
lnexenl <Shadow_8890@outlook.com>
2022-04-09 22:34:16 +0800

    prior locating fusion test successfully.

--- utils/publish_gicp_path.py
+++ utils/publish_gicp_path.py
@@ -5,7 +5,7 @@ from rospy_message_converter.json_message_converter import convert_json_to_ros_m
 
 if __name__ == '__main__':
     rospy.init_node("gicp_publish", anonymous=True)
-    gicp_pub = rospy.Publisher("gicp_path", Path, queue_size=1)
+    gicp_pub = rospy.Publisher("/gicp_path", Path, queue_size=1)
 
     with open("{}/{}".format(os.path.abspath(os.path.curdir), "path.json"), 'r') as f:
         path = j2r("nav_msgs/Path", f.read(), strict_mode=False)

8402ae19dec859158069162ec6f93bb8f9e4ad2f
lnexenl <Shadow_8890@outlook.com>
2022-05-12 22:52:51 +0800

    strange segmentation fault to be fixed.

--- vins_estimator/src/utility/visualization.cpp
+++ vins_estimator/src/utility/visualization.cpp
@@ -155,7 +155,7 @@ void pubOdometry(const Estimator &estimator, const std_msgs::Header &header) {
               << tmp_Q.x() << " "
               << tmp_Q.y() << " "
               << tmp_Q.z() << " "
-              << tmp_Q.w() << " " << endl;
+              << tmp_Q.w() << endl;
         foutC.close();
         Eigen::Vector3d tmp_T = estimator.Ps[WINDOW_SIZE];
         printf("time: %f, t: %f %f %f q: %f %f %f %f \n", header.stamp.toSec(), tmp_T.x(), tmp_T.y(), tmp_T.z(), tmp_Q.w(), tmp_Q.x(), tmp_Q.y(), tmp_Q.z());
